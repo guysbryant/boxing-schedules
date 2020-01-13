231d6bb5adc8d6019e29b2279a34ca1ec7e3e91f
Jason J Cruz <jaycru06@yahoo.com>
2020-01-13 08:31:50 +0000

    User interface started

--- lib/boxing-schedules/cli.rb
+++ lib/boxing-schedules/cli.rb
@@ -1,6 +1,12 @@
 class BoxingSchedules::CLI
 
   def start
-    puts "Hello World"
+    puts "Hello, Welcome to The Boxing Fight Schedule Generator."
+    puts "Which displayed option would you like to select:"
+    puts "\t 1. Fight dates."
+    puts "\t 2. Fight times."
+    puts "\t 3. The fighters."
+    puts "\t 4. Fight location."
+    puts "Type 'exit' to quit."
   end
 end
