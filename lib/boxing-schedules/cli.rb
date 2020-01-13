62ab91f7a5467739605c83ad1d1c0ec61ba417a5
Jason J Cruz <jaycru06@yahoo.com>
2020-01-13 08:44:24 +0000

    User interface update 1

--- lib/boxing-schedules/cli.rb
+++ lib/boxing-schedules/cli.rb
@@ -7,6 +7,26 @@ class BoxingSchedules::CLI
     puts "\t 2. Fight times."
     puts "\t 3. The fighters."
     puts "\t 4. Fight location."
-    puts "Type 'exit' to quit."
+    puts "\t Type 'exit' to quit."
+    
+    user_input = gets.strip.downcase
+    case user_input 
+    when '1'
+      # get fight dates, display them to the user.
+      "fight dates"
+    when '2'
+      # get fight times, display them to the user.
+      "fight times"
+    when '3' 
+      # get fighter names, display them to the user.
+      "fighter names"
+    when '4'
+      # get fight location/arena, display it to the user.
+      "fight location"
+    when 'exit'
+      "Goodbye!"
+    else 
+      start 
+    end 
   end
 end
