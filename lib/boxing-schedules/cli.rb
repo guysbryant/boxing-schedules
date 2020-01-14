3762f5c5afd82e1a9c44ab48fe0f03710fba00f9
Jason J Cruz <jaycru06@yahoo.com>
2020-01-14 00:47:23 +0000

    edit to user interface

--- lib/boxing-schedules/cli.rb
+++ lib/boxing-schedules/cli.rb
@@ -1,34 +1,41 @@
 class BoxingSchedules::CLI
 
-  def start
+  def main_menu
     puts "Hello, Welcome to The Boxing Fight Schedule Generator."
     puts "Which displayed option would you like to select(Type the number to select):"
-    puts "\t 1.Fight dates."
-    puts "\t 2.Fight times."
-    puts "\t 3.The fighters."
-    puts "\t 4.Fight location."
-    puts "\t Type 'exit' to quit."
+    puts "1.Fight dates."
+    puts "2.Fight times."
+    puts "3.The fighters."
+    puts "4.Fight location."
+    puts "Type 'exit' to quit."
+    puts "Type 'list' to see options again."
+  end
 
-    user_input = gets.strip.downcase
-    case user_input
-    when '1'
-      # get fight dates, display them to the user.
-      "fight dates"
-      url = "https://box.live/upcoming-fights-schedule/"
-      BoxingSchedules::Scraper.scrape_scheduled_fights(url)
-    when '2'
-      # get fight times, display them to the user.
-      "fight times"
-    when '3'
-      # get fighter names, display them to the user.
-      "fighter names"
-    when '4'
-      # get fight location/arena, display it to the user.
-      "fight location"
-    when 'exit'
-      "Goodbye!"
-    else
-      start
+  def start
+    user_input = nil
+    while user_input != 'exit'
+      main_menu
+      user_input = gets.strip.downcase
+      case user_input
+      when '1'
+        # get fight dates, display them to the user.
+        "fight dates"
+        url = "https://box.live/upcoming-fights-schedule/"
+        BoxingSchedules::Scraper.scrape_scheduled_fights(url)
+      when '2'
+        # get fight times, display them to the user.
+        "fight times"
+      when '3'
+        # get fighter names, display them to the user.
+        "fighter names"
+      when '4'
+        # get fight location/arena, display it to the user.
+        "fight location"
+      when 'exit'
+        puts "Goodbye!"
+      else
+        start
+      end
     end
   end
 end
--- lib/boxing-schedules/scraper.rb
+++ lib/boxing-schedules/scraper.rb
@@ -1,8 +1,9 @@
 class BoxingSchedules::Scraper
   # scrape site: https://box.live/upcoming-fights-schedule/
   def self.scrape_scheduled_fights(url)
+    page = Nokogiri::HTML(open(url))
     puts "in the scraper class."
     # open url and scrape all scheduled fights.
   end
-  
+
 end
